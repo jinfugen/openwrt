20fd5d9e0614652b4ca9f63ecac8184512013b4c
jinfugen <52519437+jinfugen@users.noreply.github.com>
2022-11-29 23:33:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,4 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # Clear the login password
 #sed -i 's/password//g' /package/lean/default-settings/files/zzz-default-settings
+sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
